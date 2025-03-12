264b31ed2836beef2d9d51b039046287e3df5e0f
Tas-nim3 <tasnimarafatpuspita@gmail.com>
2025-03-12 21:49:01 +0600

    Dynamic buttons are added

---
+++ script/index.js
@@ -0,0 +1,35 @@
+function loadCategories(){
+    
+    //1 fetch data 
+    fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
+
+    //2 convert promise to json
+    .then((res)=>res.json())
+
+    //3.send data to display
+    .then((data) => displayCategories(data.categories));
+}
+
+function displayCategories(categories){
+//get the container
+
+ const categoryContainer = document.getElementById("category-container");
+
+ //Loop operation on array of object
+ for(let cat of categories){
+ 
+ //create element
+ const categoryDiv = document.createElement("div");
+
+categoryDiv.innerHTML=`
+<button class="btn btn-sm hover:bg-[#FF1F3D] hover:text-white">${cat.category}</button>
+        
+`;
+//Append the element
+
+categoryContainer.appendChild(categoryDiv)
+
+ }
+}
+
+loadCategories();
